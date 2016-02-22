59e2a6637da0d3cea845ddbcb31236dfd0c41d62
jessicato <jessicato@users.noreply.github.com>
2016-02-22 13:34:45 -0800

    Added test cases from GIT.  Stupid VS.

--- UnitTest/StringReplacementTests.cs
+++ UnitTest/StringReplacementTests.cs
@@ -9,5 +9,13 @@ namespace UnitTest {
 		public void ReplaceAllSpaces() {
 			Assert.AreEqual(StringReplacement.ReplaceSpaces("Jessica Joshua Jaden Francis"), "Jessica%20Joshua%20Jaden%20Francis");
 		}
+		[TestMethod]
+		public void ReverseStringWithNumber() {
+			Assert.AreEqual(Exercise.ReverseString.ReverseStringInput("abc3de5"), "5ed3cba");
+		}
+		[TestMethod]
+		public void ReverseCharacterArray() {
+			CollectionAssert.AreEqual(Exercise.ReverseString.ReverseCharacterArray(new char[] { 'a', 'b', 'c', 'd' }), new char[] { 'd','c', 'b', 'a' }); 
+		}
 	}
 }
