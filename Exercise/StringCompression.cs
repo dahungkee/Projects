a282a1581aa35e7a5c745c84dc3fef40b4eb9991
https://github.com/jessicato/Projects.git <jessica@tofuhero.com>
2016-03-10 10:58:44 -0800

    added verson1 of StringCompression method

---
+++ Exercise/StringCompression.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise {
+	public static class StringCompression {
+		/// <summary>
+		/// Implement a method to perform basic string compression using the counts of repeated characters. 
+		/// For example, the string aabcccccaaa would become a2blc5a3. 
+		/// If the "compressed" string would not become smaller than the original string, your method should return the original string.
+		/// </summary>
+		/// <param name="input"></param>
+		/// <returns></returns>
+		public static string StringCompressMethod(string input) {
+			StringBuilder sb = new StringBuilder();			
+			int count = 1;
+			string currentLetter = input[0].ToString();
+
+			for (int i = 1; i < input.Length; i++) {				
+				if (currentLetter == input[i].ToString()) {
+					count++;					
+				} else {
+					sb.Append(currentLetter + count);
+					currentLetter = input[i].ToString();
+					count = 1;
+				}
+				if (i == input.Length - 1) {
+					sb.Append(currentLetter + count);
+				}
+			}
+
+			return sb.Length > input.Length ? input : sb.ToString();
+		}
+	}
+}
---
+++ UnitTest/StringCompressionTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise;
+
+namespace UnitTest {
+	[TestClass]
+	public class StringCompressionTests {
+		[TestMethod]
+		public void StringCompressTest() {
+			Assert.AreEqual(Exercise.StringCompression.StringCompressMethod("aabcccccaaa"), "a2b1c5a3");
+		}
+		[TestMethod]
+		public void StringCompressTest2() {
+			Assert.AreEqual(Exercise.StringCompression.StringCompressMethod("abc"), "abc");
+		}
+	}
+}
